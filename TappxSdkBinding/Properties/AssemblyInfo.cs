b72b0ec6f910ae44f13befcf49d95cea9a0bdd46
MrTapioca <46765273+MrTapioca@users.noreply.github.com>
2020-07-04 18:31:27 -0400

    Add project files.

---
+++ TappxSdkBinding/Properties/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Android.App;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TappxSdkBinding")]
+[assembly: AssemblyDescription("Simple Bindings Library of Tappx SDK 3.1.9 for Xamarin.Android")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Natan Tapia (Bindings Library), Tappx (SDK)")]
+[assembly: AssemblyProduct("TappxSdkBinding")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+[assembly: AssemblyVersion("3.1.9")]
+[assembly: AssemblyFileVersion("3.1.9")]
